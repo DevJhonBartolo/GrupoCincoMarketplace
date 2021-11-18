a498856874dc616b67fdead72eb68e8b8f8ad861
Jhon Mario Bartolo Arcila <jhonmario1980@gmail.com>
2021-11-18 14:16:01 -0500

    creacion toolbar e inicio navigation drawer

--- app/src/main/java/com/jhondevs/grupocincomarketplace/MainActivity.kt
+++ app/src/main/java/com/jhondevs/grupocincomarketplace/MainActivity.kt
@@ -5,21 +5,45 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        title="BioSun"
         super.onCreate(savedInstanceState)
-        this.supportActionBar?.hide()
+        //this.supportActionBar?.hide()
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.my_toolbar))
 
 
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-        val adapter = CustomAdapter()
+        //val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        //val adapter = CustomAdapter()
 
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = adapter
+        //recyclerView.layoutManager = LinearLayoutManager(this)
+        //recyclerView.adapter = adapter
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main_activity,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean =when(item.itemId) {
+        R.id.action_search -> {
+            Toast.makeText(this, R.string.txt_action_search,Toast.LENGTH_LONG).show()
+            true
+        }
+        R.id.action_settings -> {
+            Toast.makeText(this, R.string.txt_action_settings,Toast.LENGTH_LONG).show()
+            true
+        }
+        R.id.action_logout -> {
+            Toast.makeText(this, R.string.txt_action_logout,Toast.LENGTH_LONG).show()
+            true
+        }
+        else -> {
+            super.onOptionsItemSelected(item)
+        }
+    }
 }
